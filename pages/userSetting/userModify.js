b2a26e0a17c66e1ccdea9e15173660eec220ed44
lt14 <592737616@qq.com>
2018-12-17 17:21:26 +0800

    修改姓氏/性别页面

--- pages/userSetting/userModify.js
+++ pages/userSetting/userModify.js
@@ -1,43 +1,29 @@
-// pages/userSetting/userSetting.js
-const network = require("../../utils/network.js")
-const {
-  api
-} = require("../../utils/config.js")
-const timeApi = require('../../utils/util.js');
-const app = getApp()
-
+// pages/userSetting/userModify.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo: {}
+    value: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    network.GET({
-      // url: api.getUserById + app.globaldata.userId,
-      url: api.getUserById + "1",
-      success: res => {
-        if (res.success) {
-          res.content.joinDate = timeApi.formatDateAndTime(new Date(res.content.joinDate));
-          res.content.gender = res.content.gender ? "女" : "男";
-          this.setData({
-            userInfo: res.content
-          });
-        } else {
-          wx.showToast({
-            title: '查询失败',
-            icon: 'none',
-            duration: 5000
-          })
-        }
-      }
-    })
+    this.setData({
+      value: options.value
+    });
+    if (options.type == "name") {
+      wx.setNavigationBarTitle({
+        title: '修改姓氏'
+      })
+    } else if (options.type == "gender") {
+      wx.setNavigationBarTitle({
+        title: '修改性别'
+      })
+    }
   },
 
   /**
