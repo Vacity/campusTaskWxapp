282cd022a3ceb7016b564def4421b2fda6a3609e
Vacity <141250206@smail.nju.edu.com>
2018-12-18 16:35:19 +0800

    获取授权

--- pages/index/index.js
+++ pages/index/index.js
@@ -47,6 +47,80 @@ Page({
     })
   },
   onLoad: function () {
+    wx.getSetting({
+      success: res => {
+        if (!res.authSetting['scope.userInfo']) {
+          wx.showModal({
+            title: '注意',
+            showCancel: false,
+            confirmText: '授权',
+            content: '为了您更好的体验,请先同意授权',
+            success: function (res) {
+              wx.navigateTo({
+                url: '../index/right'
+              });
+            }
+          })
+        }
+      }
+    })
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        var scope = this;
+        // 登录
+        wx.login({
+          success: res => {
+            network.GET({
+              url: api.loginsession + "?appid=wx794fcc0f508c628f&secret=3ef2b04022c3108f6d5a0fc781e9db37&js_code=" + res.code + "&grant_type=authorization_code",
+              success: res2 => {
+                network.GET({
+                  url: api.login + res2.openid,
+                  success: response => {
+                    if (response.success) {
+                      if (response.content)
+                        app.globalData.user = response.content;
+                      else {
+                        var infoRes = app.globalData;
+                        var info = infoRes.userInfo
+                        network.POST({
+                          url: api.register,
+                          data: {
+                            username: res2.openid,
+                            nickname: infoRes.userInfo.nickName,
+                            gender: infoRes.userInfo.gender,
+                            avatar: infoRes.userInfo.avatarUrl
+                          },
+                          success: registerResponse => {
+                            if (registerResponse.success == true) {
+                              app.globalData.user = registerResponse.content;
+                            } else {
+                              wx.showToast({
+                                title: '注册失败',
+                                icon: 'none',
+                                duration: 5000
+                              })
+                            }
+                          }
+                        })
+                      }
+                    } else {
+                      wx.showToast({
+                        title: '登录失败',
+                        icon: 'none',
+                        duration: 5000
+                      })
+                    }
+                  }
+                })
+              }
+            })
+          }
+        })
+      }
+    })
+
     network.GET({
       url: api.getTaskByType + "DELIVER",
       success: res => {
